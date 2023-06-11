c1c6c9f6f728314e23a728c7a30c80fbef8737a2
Asas Waheed <asas.waheed@klarna.com>
2023-06-11 05:37:31 +0200

    fixed basic design of daily log screen

--- src/screens/DailyLogScreen.js
+++ src/screens/DailyLogScreen.js
@@ -18,35 +18,55 @@ const DailyLogScreen = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.summaryContainer}>
-        <Text style={styles.headerText}>Daily Calorie Intake</Text>
-        <Text style={styles.calorieText}>{dailyCalories} kcal</Text>
-        <Text style={styles.goalText}>Goal: {dailyGoal} kcal</Text>
-        <Text style={styles.goalText}>Calories Left: {caloriesLeft} kcal</Text>
-        <View style={styles.macronutrientContainer}>
-          <Text style={styles.macronutrientText}>Carbs: {macronutrients.carbs} g</Text>
-          <Text style={styles.macronutrientText}>Protein: {macronutrients.protein} g</Text>
-          <Text style={styles.macronutrientText}>Fat: {macronutrients.fat} g</Text>
+      <View
+        style={[
+          styles.wrapperContainer,
+          {backgroundColor: '#ebe6e6'},
+        ]}>
+        <View style={[styles.summaryContainer]}>
+          <Text style={styles.headerText}>Gegessen</Text>
+          <Text style={[styles.calorieText, {color: (dailyGoal - dailyCalories) < 0 ? '#a10808' : '#4ecf04'}]}>{dailyCalories} kcal</Text>
+          <Text style={styles.goalText}>Goal: {dailyGoal} kcal</Text>
+          <Text style={styles.goalText}>
+            Calories Left: {caloriesLeft} kcal
+          </Text>
+          <View style={styles.macronutrientContainer}>
+            <Text style={styles.macronutrientText}>
+              Carbs: {macronutrients.carbs} g
+            </Text>
+            <Text style={styles.macronutrientText}>
+              Protein: {macronutrients.protein} g
+            </Text>
+            <Text style={styles.macronutrientText}>
+              Fat: {macronutrients.fat} g
+            </Text>
+          </View>
         </View>
       </View>
 
       <View style={styles.mealContainer}>
         <View style={styles.mealItem}>
-          <Text style={styles.mealText}>Breakfast - {mealLogs.breakfast} kcal</Text>
+          <Text style={styles.mealText}>
+            Frühstück - {mealLogs.breakfast} kcal
+          </Text>
           <TouchableOpacity onPress={() => navigation.navigate('BreakfastLog')}>
-            <Text style={styles.editText}>Edit</Text>
+            <Text style={styles.editText}>Bearbeiten</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.mealItem}>
-          <Text style={styles.mealText}>Lunch - {mealLogs.lunch} kcal</Text>
+          <Text style={styles.mealText}>
+            Mittagessen - {mealLogs.lunch} kcal
+          </Text>
           <TouchableOpacity onPress={() => navigation.navigate('LunchLog')}>
-            <Text style={styles.editText}>Edit</Text>
+            <Text style={styles.editText}>Bearbeiten</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.mealItem}>
-          <Text style={styles.mealText}>Dinner - {mealLogs.dinner} kcal</Text>
+          <Text style={styles.mealText}>
+            Abendessen - {mealLogs.dinner} kcal
+          </Text>
           <TouchableOpacity onPress={() => navigation.navigate('DinnerLog')}>
-            <Text style={styles.editText}>Edit</Text>
+            <Text style={styles.editText}>Bearbeiten</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -59,6 +79,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffffff',
   },
+  wrapperContainer: {
+    flex: 0.7,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    margin: 7,
+    borderWidth: 0.5, // Set the border width (e.g., 1 pixel)
+    borderColor: '#5e615f', // Set the border color (e.g., black)
+    borderRadius: 20, // Set the border radius (e.g., 10 pixels) for rounded corners
+  },
   summaryContainer: {
     flex: 0.25,
     justifyContent: 'center',
@@ -68,7 +98,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: '#333',
-    marginBottom: 10,
+    marginBottom: 5,
   },
   calorieText: {
     fontSize: 48,
@@ -85,15 +115,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%',
-    paddingHorizontal: 20,
+    paddingHorizontal: 2,
   },
   macronutrientText: {
     fontSize: 18,
     color: '#333',
   },
   mealContainer: {
-    flex: 0.75,
+    flex: 0.8,
     paddingHorizontal: 20,
+    paddingTop: 10,
   },
   mealItem: {
     flexDirection: 'row',
