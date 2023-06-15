58813881f58a4ff2e2e9ccccf1aa07324d5e75ab
Asas Waheed <asas.waheed@klarna.com>
2023-06-15 22:15:28 +0200

    styling

--- src/components/BasicButton.js
+++ src/components/BasicButton.js
@@ -4,7 +4,7 @@ import { View, StyleSheet, Button } from 'react-native';
 export default function BasicButton({ style, title, onPress }) {
   return (
     <View style={[style, { margin: 5 }]}>
-      <Button title={title} onPress={onPress} />
+      <Button title={title} onPress={onPress} color="#262523" />
     </View>
   );
 }
